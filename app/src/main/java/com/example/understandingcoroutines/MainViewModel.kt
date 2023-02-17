909deaa82e279b3d8e580fc5bcabd7f79e9da61c
Vlad <ricean.vlad7@gmail.com>
2023-02-17 09:26:31 +0200

    All types of channels

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -1,19 +1,79 @@
 package com.example.understandingcoroutines
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 
 class MainViewModel : ViewModel() {
 
     init {
-        main()
+//        rendezvousChannel()
+//        unlimitedChannel()
+//        conflatedChannel()
+//        bufferedChannel()
     }
 
-    private fun main() = runBlocking {
-       val channel = Channel<String>()
+    private fun bufferedChannel() = runBlocking{
+        val channel = Channel<String>(2)
+        launch {
+            channel.send("A1")
+            channel.send("A2")
+            println("A done")
+        }
+        launch {
+            channel.send("B1")
+            println("B done")
+        }
+        launch {
+            repeat(5) {
+                val x = channel.receive()
+                println("Received: $x")
+            }
+        }
+    }
+
+    private fun conflatedChannel() = runBlocking{
+        val channel = Channel<String>(CONFLATED)
+        launch {
+            channel.send("A1")
+            channel.send("A2")
+            println("A done")
+        }
+        launch {
+            channel.send("B1")
+            println("B done")
+        }
+        launch {
+            repeat(5) {
+                val x = channel.receive()
+                println("Received: $x")
+            }
+        }
+    }
+
+    private fun unlimitedChannel() = runBlocking{
+        val channel = Channel<String>(UNLIMITED)
+        launch {
+            channel.send("A1")
+            channel.send("A2")
+            println("A done")
+        }
+        launch {
+            channel.send("B1")
+            println("B done")
+        }
+        launch {
+            repeat(5) {
+                val x = channel.receive()
+                println("Received: $x")
+            }
+        }
+    }
+
+    private fun rendezvousChannel() = runBlocking {
+        val channel = Channel<String>()
         launch {
             channel.send("A1")
             channel.send("A2")
