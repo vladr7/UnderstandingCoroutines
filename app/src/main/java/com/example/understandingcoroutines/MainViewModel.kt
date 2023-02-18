eb4fcce2a1234507be77f05c5ca6737d7c9452b2
Vlad <ricean.vlad7@gmail.com>
2023-02-18 13:57:47 +0200

    Flow filter
    https://kotlinlang.org/docs/flow.html#flows-are-sequential

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -19,10 +19,17 @@ class MainViewModel : ViewModel() {
 
 
     fun main() = runBlocking<Unit> {
-        val sum = (1..5).asFlow()
-            .map { it * it } // squares of numbers from 1 to 5
-            .reduce { a, b -> a + b } // sum them (terminal operator)
-        println(sum)
+        (1..5).asFlow()
+            .filter {
+                println("Filter $it")
+                it % 2 == 0
+            }
+            .map {
+                println("Map $it")
+                "string $it"
+            }.collect {
+                println("Collect $it")
+            }
     }
 
 }
