70fdf1eb86f634fd0b1aedc02c39c782a9cef092
Vlad <ricean.vlad7@gmail.com>
2023-02-18 15:20:41 +0200

    Flow cancellation
    https://kotlinlang.org/docs/flow.html#flow-cancellation-checks

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -18,15 +18,19 @@ class MainViewModel : ViewModel() {
         main()
     }
 
-    fun simple(): Flow<Int> = (1..3).asFlow()
 
-    fun main() = runBlocking<Unit> {
-        try {
-            simple().collect { value -> println(value) }
-        } finally {
-            println("Done")
+    fun foo(): Flow<Int> = flow {
+        for (i in 1..5) {
+            println("Emitting $i")
+            emit(i)
         }
     }
 
+    fun main() = runBlocking<Unit> {
+        foo().collect { value ->
+            if (value == 3) cancel()
+            println(value)
+        }
+    }
 
 }
