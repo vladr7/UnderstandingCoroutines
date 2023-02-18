4b32cf6561c9289710c5a7eec33c7ccbb1e8c72f
Vlad <ricean.vlad7@gmail.com>
2023-02-18 13:55:37 +0200

    Flow terminal operators (reduce)

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -19,20 +19,10 @@ class MainViewModel : ViewModel() {
 
 
     fun main() = runBlocking<Unit> {
-        numbers()
-            .take(2) // take only the first two
-            .collect { value -> println(value) }
-    }
-
-    fun numbers(): Flow<Int> = flow {
-        try {
-            emit(1)
-            emit(2)
-            println("This line will not execute")
-            emit(3)
-        } finally {
-            println("Finally in numbers")
-        }
+        val sum = (1..5).asFlow()
+            .map { it * it } // squares of numbers from 1 to 5
+            .reduce { a, b -> a + b } // sum them (terminal operator)
+        println(sum)
     }
 
 }
