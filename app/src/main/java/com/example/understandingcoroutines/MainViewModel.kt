5b16039e5a5dbf82e2a5f3e179d741c6580e93b1
Vlad <ricean.vlad7@gmail.com>
2023-02-18 13:33:52 +0200

    Cold Flow simple example
    https://kotlinlang.org/docs/flow.html#flows-are-cold

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -1,34 +1,39 @@
 package com.example.understandingcoroutines
 
+import android.icu.number.Notation.simple
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.flow.*
+import java.lang.Math.log
 import kotlin.coroutines.resume
+import kotlin.system.measureTimeMillis
 
 class MainViewModel : ViewModel() {
 
     init {
-        viewModelScope.launch {
-            val task1 = async { doSomethingUsefulOne() }
-            val task2 = async { doSomethingUsefulTwo() }
-            val result1 = task1.await()
-            val result2 = task2.await()
-            println("${result1 + result2}")
+        main()
+    }
+
+    fun simple(): Flow<Int> = flow {
+        println("Flow started")
+        for (i in 1..3) {
+            delay(100)
+            emit(i)
         }
     }
 
-    suspend fun doSomethingUsefulOne(): Int {
-        delay(2000L) // pretend we are doing something useful here
-        println("Done")
-        return 13
+    fun main() = runBlocking<Unit> {
+        println("Calling simple function...")
+        val flow = simple()
+        println("Calling collect...")
+        flow.collect { value -> println(value) }
+        println("Calling collect again...")
+        flow.collect { value -> println(value) }
     }
 
-    suspend fun doSomethingUsefulTwo(): Int {
-        delay(2000L) // pretend we are doing something useful here, too
-        println("Done")
-        return 29
-    }
+
 }
