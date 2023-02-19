387055dcee93f08f62eeb26d98f6731421f2f329
Vlad <ricean.vlad7@gmail.com>
2023-02-19 13:16:56 +0200

    Coroutine Exception Handler (childs delegate handling of their exceptions to their parent coroutine, which also delegates to the parent, and so on until the root)
    https://kotlinlang.org/docs/exception-handling.html#cancellation-and-exceptions

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -1,45 +1,55 @@
 package com.example.understandingcoroutines
 
-import android.icu.number.Notation.simple
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
-import kotlinx.coroutines.GlobalScope.coroutineContext
-import kotlinx.coroutines.channels.*
-import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
-import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
-import kotlinx.coroutines.flow.*
-import java.lang.System.currentTimeMillis
-import kotlin.coroutines.resume
-import kotlin.system.measureTimeMillis
 
 class MainViewModel : ViewModel() {
 
     init {
         main()
+//        fromDocs()
     }
 
-    fun main() = runBlocking<Unit> {
-        val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
-        var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
-        println("Initial element is available immediately: $nextElement") // no initial delay
+    fun main() = runBlocking {
+        val handler = CoroutineExceptionHandler { coroutineContext, exception ->
+            println("CoroutineExceptionHandler: $exception")
+        }
+        viewModelScope.launch(handler) {
+            delay(1000L)
+            launch {
+                delay(500L)
+                println("Coroutine 1")
+            }
+            launch {
+                delay(100L)
+                throw ArithmeticException()
+            }
+        }
+    }
 
-        nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements have 100ms delay
-        println("Next element is not ready in 50 ms: $nextElement")
-
-        nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
-        println("Next element is ready in 100 ms: $nextElement")
-
-        // Emulate large consumption delays
-        println("Consumer pauses for 150ms")
-        delay(150)
-        // Next element is available immediately
-        nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
-        println("Next element is available immediately after large consumer delay: $nextElement")
-        // Note that the pause between `receive` calls is taken into account and next element arrives faster
-        nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
-        println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
-
-        tickerChannel.cancel() // indicate that no more elements are needed
+    private fun fromDocs() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("CoroutineExceptionHandler got $exception")
+        }
+        val job = GlobalScope.launch(handler) {
+            launch { // the first child
+                try {
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    withContext(NonCancellable) {
+                        println("Children are cancelled, but exception is not handled until all children terminate")
+                        delay(100)
+                        println("The first child finished its non cancellable block")
+                    }
+                }
+            }
+            launch { // the second child
+                delay(10)
+                println("Second child throws an exception")
+                throw ArithmeticException()
+            }
+        }
+        job.join()
     }
 }
