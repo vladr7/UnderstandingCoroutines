d1a7c14b2dc55dd3d8308c4f7aad6d0a0972d026
Vlad <ricean.vlad7@gmail.com>
2023-02-19 11:07:17 +0200

    Flow channel produce & consume
    https://kotlinlang.org/docs/channels.html#building-channel-producers

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.flow.*
 import java.lang.System.currentTimeMillis
 import kotlin.coroutines.resume
@@ -19,18 +22,12 @@ class MainViewModel : ViewModel() {
     }
 
     fun main() = runBlocking<Unit> {
-        val channel = Channel<Int>()
-        launch {
-            // this might be heavy CPU-consuming computation or async logic, we'll just send five squares
-            for (x in 1..5) {
-                delay(500L)
-                channel.send(x * x)
-            }
-//            channel.close()
-        }
-        // here we print five received integers:
-        repeat(5) { println(channel.receive()) }
+        val squares = produceSquares()
+        squares.consumeEach { println(it) }
         println("Done!")
     }
 
+    fun CoroutineScope.produceSquares(): ReceiveChannel<Int> = produce {
+        for (x in 1..5) send(x * x)
+    }
 }
