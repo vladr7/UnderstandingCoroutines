2c6eed3c33a3c6b686fe66d3a3140aab4cb22860
Vlad <ricean.vlad7@gmail.com>
2023-02-20 09:57:54 +0200

    SharedFlow - replay explained

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.understandingcoroutines
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
@@ -11,9 +12,9 @@ class MainViewModel : ViewModel() {
         state()
     }
 
-    fun sharedFunc(): MutableSharedFlow<Boolean> {
+    fun sharedFunc(): MutableSharedFlow<Int> {
         println("Flow started")
-        return MutableSharedFlow<Boolean>(replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST) // this is EXACTLY a StateFlow (without initial value)
+        return MutableSharedFlow<Int>(replay = 2, onBufferOverflow = BufferOverflow.DROP_LATEST) // this is EXACTLY a StateFlow (without initial value)
         /**
          * Use SharedFlow when you need a StateFlow with tweaks in its behavior such as extra buffering, replaying more values, or omitting the initial value.
          */
@@ -22,11 +23,23 @@ class MainViewModel : ViewModel() {
     private fun state() = runBlocking<Unit> {
         println("calling sharedFunc")
         val sharedFlow = sharedFunc()
-        sharedFlow.emit(true)
-        delay(100L)
-        println("Calling collect...")
-        sharedFlow.collect { value -> println(value) }
-        println("Calling collect again...")
-        sharedFlow.collect { value -> println(value) }
+        delay(1100L)
+        sharedFlow.tryEmit(1)
+        sharedFlow.tryEmit(2)
+        sharedFlow.tryEmit(3)
+        sharedFlow.tryEmit(4)
+        viewModelScope.launch {
+            launch {
+                println("Calling collect...")
+                sharedFlow.collect { value -> println(value) }
+            }
+            launch {
+                println("Calling collect...")
+                sharedFlow.collect { value -> println(value) }
+            }
+        }
+
+//        println("Calling collect again...")
+//        sharedFlow.collect { value -> println(value) }
     }
 }
