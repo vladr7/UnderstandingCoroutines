3f2d0741ea4720487d5dc820721f54e7148759ea
Vlad <ricean.vlad7@gmail.com>
2023-02-20 09:43:01 +0200

    StateFlow hot flow example

--- app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
+++ app/src/main/java/com/example/understandingcoroutines/MainViewModel.kt
@@ -1,40 +1,54 @@
 package com.example.understandingcoroutines
 
+import android.icu.number.Notation.simple
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.flow.*
+import java.lang.Math.log
+import kotlin.coroutines.resume
+import kotlin.system.measureTimeMillis
 
 class MainViewModel : ViewModel() {
 
     init {
-        main()
+//        main()
+        state()
     }
 
-    fun main() = runBlocking {
-        val supervisor = SupervisorJob()
-        with(CoroutineScope(coroutineContext + supervisor)) {
-            // launch the first child -- its exception is ignored for this example (don't do this in practice!)
-            val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
-                println("The first child is failing")
-                throw AssertionError("The first child is cancelled")
-            }
-            // launch the second child
-            val secondChild = launch {
-                firstChild.join()
-                // Cancellation of the first child is not propagated to the second child
-                println("The first child is cancelled: ${firstChild.isCancelled}, but the second one is still active")
-                try {
-                    delay(Long.MAX_VALUE)
-                } finally {
-                    // But cancellation of the supervisor is propagated
-                    println("The second child is cancelled because the supervisor was cancelled")
-                }
-            }
-            // wait until the first child fails & completes
-            firstChild.join()
-            println("Cancelling the supervisor")
-            supervisor.cancel()
-            secondChild.join()
+    fun stateFunc(): MutableStateFlow<Boolean> {
+        println("Flow started")
+        return MutableStateFlow<Boolean>(true)
+    }
+
+    private fun state() = runBlocking<Unit> {
+        println("calling stateFunc")
+        val stateFlow = stateFunc()
+        println("Calling collect...")
+        stateFlow.collect { value -> println(value) }
+        println("Calling collect again...")
+        stateFlow.collect { value -> println(value) }
+    }
+
+    fun simple(): Flow<Int> = flow {
+        println("Flow started")
+        for (i in 1..3) {
+            delay(100)
+            emit(i)
         }
     }
+
+    fun main() = runBlocking<Unit> {
+        println("Calling simple function...")
+        val flow = simple()
+        println("Calling collect...")
+        flow.collect { value -> println(value) }
+        println("Calling collect again...")
+        flow.collect { value -> println(value) }
+    }
+
+
 }
